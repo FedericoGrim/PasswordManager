d92c6753c9a7d501ed729b3b7922574b87d2d282
FedericoGrim <federico.grimaldi2006@gmail.com>
2025-05-12 23:10:39 +0200

    Removal of empty spaces

--- password-manager_backend/main.py
+++ password-manager_backend/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-
 from Presentation.UserAPI import router as user_router
 
-App = FastAPI()
-
-App.include_router(user_router, prefix="/users", tags=["user"])
+app = FastAPI()
+app.include_router(user_router, prefix="/users", tags=["user"])
