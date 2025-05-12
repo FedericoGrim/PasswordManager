02fc745b548714e0ffc8f3ec6515f24d6989cc67
FedericoGrim <federico.grimaldi2006@gmail.com>
2025-05-12 23:09:08 +0200

    Correction of the UserInterface

--- password-manager_backend/Application/UserInterface.py
+++ password-manager_backend/Application/UserInterface.py
@@ -1,11 +1,20 @@
-def InterfaceCreateUser():
-    pass
+import uuid
+from fastapi import Depends
+from sqlalchemy.orm import Session
 
-def InterfaceUpdateUser():
-    pass
+# Importa il domain layer, senza fare importazioni dirette dal DB.
+from Domain.UserService import get_db
+from Domain.Objects.UserObj import UserCreate, UserUpdate
+from Domain.IUserService import CreateUser, GetUser, UpdateUser, DeleteUser
 
-def InterfaceDeleteUser():
-    pass
+def InterfaceCreateUser(user: UserCreate, db: Session = Depends(get_db)):
+    return CreateUser(db, user)
 
-def InterfaceGetUser():
-    pass
+def InterfaceGetUser(userEmail: str, db: Session = Depends(get_db)):
+    return GetUser(db, userEmail)
+
+def InterfaceUpdateUser(user_id: uuid.UUID, user: UserUpdate, db: Session = Depends(get_db)):
+    return UpdateUser(db, user_id, user)
+
+def InterfaceDeleteUser(user_id: uuid.UUID, db: Session = Depends(get_db)):
+    return DeleteUser(db, user_id)
