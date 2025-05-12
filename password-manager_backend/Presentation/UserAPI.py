aab439513d124be6f508237926ad699da6edd704
FedericoGrim <federico.grimaldi2006@gmail.com>
2025-05-12 23:09:44 +0200

    Correction of naming conventions and useless code

--- password-manager_backend/Presentation/UserAPI.py
+++ password-manager_backend/Presentation/UserAPI.py
@@ -1,36 +1,37 @@
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException
 from Domain.Objects.UserObj import UserCreate, UserUpdate
 import uuid
-
 from Application.UserInterface import InterfaceDeleteUser, InterfaceGetUser, InterfaceCreateUser, InterfaceUpdateUser
 
 router = APIRouter()
 
 @router.post("/")
 async def CreateUser(user: UserCreate):
-    if InterfaceCreateUser(user):
-        return {"message": "User created successfully"}
+    created_user = InterfaceCreateUser(user)
+    if created_user:
+        return {"message": "User created successfully", "user": created_user}  # Restituire i dettagli dell'utente creato
     else:
         raise HTTPException(status_code=400, detail="User creation failed")
 
 @router.get("/")
-async def get_user(user_email: str = Query(..., description="Email of the user to retrieve")):
-    user = InterfaceGetUser(user_email) 
+async def GetUser(userEmail: str):
+    user = InterfaceGetUser(userEmail)
     if user:
         return {"message": "User retrieved successfully", "user": user}
     else:
         raise HTTPException(status_code=404, detail="User not found")
 
 @router.put("/{user_id}")
-async def UpdateUser(user_id: uuid.UUID, user: UserUpdate):
-    if InterfaceUpdateUser(user_id, user): 
-        return {"message": "User updated successfully"}
+async def UpdateUser(userId: uuid.UUID, user: UserUpdate):
+    updated_user = InterfaceUpdateUser(userId, user)
+    if updated_user:
+        return {"message": "User updated successfully", "user": updated_user}  # Restituire i dettagli dell'utente aggiornato
     else:
         raise HTTPException(status_code=400, detail="User update failed")
 
 @router.delete("/{user_id}")
-async def DeleteUser(user_id: uuid.UUID):
-    if InterfaceDeleteUser(user_id):  
+async def DeleteUser(userId: uuid.UUID):
+    if InterfaceDeleteUser(userId):
         return {"message": "User deleted successfully"}
     else:
         raise HTTPException(status_code=400, detail="User deletion failed")
