2ed7da9b7583e350beb7e1787e7907ee65d43e51
GrimFede <federico.grimaldi2006@gmail.com>
2025-05-13 12:39:06 +0200

    Improoving naming convention

--- password-manager_backend/Domain/IUserService.py
+++ password-manager_backend/Domain/IUserService.py
@@ -4,14 +4,14 @@ from sqlalchemy.orm import Session
 from Domain.Objects.UserObj import UserCreate, UserUpdate
 from Domain import UserService as UserServiceImpl
 
-def CreateUser(db: Session, user: UserCreate):
+def ICreateUser(db: Session, user: UserCreate):
     return UserServiceImpl.CreateUser(db, user)
 
-def GetUser(db: Session, userEmail: str):
+def IGetUser(db: Session, userEmail: str):
     return UserServiceImpl.GetUser(db, userEmail)
 
-def UpdateUser(db: Session, userId: uuid.UUID, user: UserUpdate):
+def IUpdateUser(db: Session, userId: uuid.UUID, user: UserUpdate):
     return UserServiceImpl.UpdateUser(db, userId, user)
 
-def DeleteUser(db: Session, userId: uuid.UUID):
+def IDeleteUser(db: Session, userId: uuid.UUID):
     return UserServiceImpl.DeleteUser(db, userId)
