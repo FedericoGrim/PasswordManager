4e5a608aa841285db862f00109166bfa1b02e1a1
GrimFede <federico.grimaldi2006@gmail.com>
2025-05-13 12:38:54 +0200

    Correction of a problematich with the connection to the DB

--- password-manager_backend/Presentation/UserAPI.py
+++ password-manager_backend/Presentation/UserAPI.py
@@ -1,37 +1,41 @@
-from fastapi import APIRouter, HTTPException
-from Domain.Objects.UserObj import UserCreate, UserUpdate
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
 import uuid
+
+from Domain.Objects.UserObj import UserCreate, UserUpdate
+from Domain.UserService import get_db
 from Application.UserInterface import InterfaceDeleteUser, InterfaceGetUser, InterfaceCreateUser, InterfaceUpdateUser
 
 router = APIRouter()
 
 @router.post("/")
-async def CreateUser(user: UserCreate):
-    createdUser = InterfaceCreateUser(user)
+async def ApiCreateUser(user: UserCreate, db: Session = Depends(get_db)):
+    print(f"User type: {type(user)}") 
+    createdUser = InterfaceCreateUser(user, db)
     if createdUser:
         return {"message": "User created successfully", "user": createdUser}
     else:
         raise HTTPException(status_code=400, detail="User creation failed")
 
 @router.get("/")
-async def GetUser(userEmail: str):
-    user = InterfaceGetUser(userEmail)
+async def ApiGetUser(userEmail: str, db: Session = Depends(get_db)):
+    user = InterfaceGetUser(userEmail, db)
     if user:
         return {"message": "User retrieved successfully", "user": user}
     else:
         raise HTTPException(status_code=404, detail="User not found")
 
 @router.put("/{user_id}")
-async def UpdateUser(userId: uuid.UUID, userUpdated: UserUpdate):
-    updatedUser = InterfaceUpdateUser(userId, userUpdated)
+async def ApiUpdateUser(userId: uuid.UUID, userUpdated: UserUpdate, db: Session = Depends(get_db)):
+    updatedUser = InterfaceUpdateUser(userId, userUpdated, db)
     if updatedUser:
         return {"message": "User updated successfully", "user": updatedUser}
     else:
         raise HTTPException(status_code=400, detail="User update failed")
 
 @router.delete("/{user_id}")
-async def DeleteUser(userId: uuid.UUID):
-    if InterfaceDeleteUser(userId):
+async def ApiDeleteUser(userId: uuid.UUID, db: Session = Depends(get_db)):
+    if InterfaceDeleteUser(userId, db):
         return {"message": "User deleted successfully"}
     else:
         raise HTTPException(status_code=400, detail="User deletion failed")
