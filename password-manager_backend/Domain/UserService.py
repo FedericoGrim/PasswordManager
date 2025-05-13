cf773d19149bea71e6e1936b77ca0d5d1fd6d624
GrimFede <federico.grimaldi2006@gmail.com>
2025-05-13 12:38:23 +0200

    Some small corrections and some type specification

--- password-manager_backend/Domain/UserService.py
+++ password-manager_backend/Domain/UserService.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from fastapi import HTTPException
 import os
 import uuid
@@ -12,7 +12,7 @@ load_dotenv()
 DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 def get_db():
     db = SessionLocal()
@@ -23,17 +23,17 @@ def get_db():
     finally:
         db.close()
 
-def CreateUser(dbSession, user: UserCreate):
+def CreateUser(dbSession : Session, user: UserCreate):
     newUser = User(**user.dict())
     dbSession.add(newUser)
     dbSession.commit()
     dbSession.refresh(newUser)
     return newUser
 
-def GetUser(dbSession, userEmail: str):
+def GetUser(dbSession : Session, userEmail: str):
     return dbSession.query(User).filter(User.email == userEmail).first()
 
-def UpdateUser(dbSession, userId: uuid.UUID, updatedUser: UserUpdate):
+def UpdateUser(dbSession : Session, userId: uuid.UUID, updatedUser: UserUpdate):
     user = dbSession.query(User).filter(User.id == userId).first()
     if user:
         if updatedUser.username:
@@ -46,7 +46,7 @@ def UpdateUser(dbSession, userId: uuid.UUID, updatedUser: UserUpdate):
         dbSession.refresh(user)
     return user
 
-def DeleteUser(dbSession, userId: uuid.UUID):
+def DeleteUser(dbSession : Session, userId: uuid.UUID):
     user = dbSession.query(User).filter(User.id == userId).first()
     if user:
         dbSession.delete(user)
