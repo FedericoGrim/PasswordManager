896d04cd22122900c6b3783eb6ddf91a993970b8
FedericoGrim <federico.grimaldi2006@gmail.com>
2025-06-01 23:23:10 +0200

    Creation of the DB query methods interfaces

---
+++ password-manager_backend/Domain/ISubAccountService.py
@@ -0,0 +1,22 @@
+import uuid
+from abc import ABC, abstractmethod
+
+from Domain.Entities.SubAccount import SubAccount
+
+@abstractmethod
+class ISubaccountService(ABC):
+    @abstractmethod
+    def CreateSubAccount(self, subaccount: SubAccount) -> dict:
+        pass
+
+    @abstractmethod
+    def GetAllSubAccountsByUserId(self, userId: uuid.UUID) -> dict:
+        pass
+
+    @abstractmethod
+    def UpdateSubAccountById(self, subaccountId: uuid.UUID, new_subaccount: SubAccount) -> dict:
+        pass
+
+    @abstractmethod
+    def DeleteSubAccountById(self, subaccountId: uuid.UUID) -> dict:
+        pass
