718f33fa6587c73149015737a192d3f9483f88d5
FedericoGrim <federico.grimaldi2006@gmail.com>
2025-06-01 23:20:44 +0200

    Removal of unusable type in db query

--- password-manager_backend/Application/DTO/SubaccountDTO.py
+++ password-manager_backend/Application/DTO/SubaccountDTO.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel, UUID4, HttpUrl, StringConstraints
 from typing import Optional, Annotated
 
 
-class CreateSubaccountDTO(BaseModel):
+class CreateSubAccountDTO(BaseModel):
     userId: UUID4
     title: Annotated[str, StringConstraints(min_length=1)]
     username: str
     passwordEncrypted: str
-    url: HttpUrl
+    url: str
 
-class UpdateSubaccountDTO(BaseModel):
+class UpdateSubAccountDTO(BaseModel):
     title: Optional[Annotated[str, StringConstraints(min_length=1)]] = None
     username: Optional[str] = None
     passwordEncrypted: Optional[str] = None
-    url: Optional[HttpUrl] = None
+    url: Optional[str] = None
