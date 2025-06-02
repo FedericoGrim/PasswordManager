03d604837900ae363ab62b6bb3435d32a8665d17
FedericoGrim <federico.grimaldi2006@gmail.com>
2025-06-02 17:06:08 +0200

    Update of relations between entities

--- password-manager_backend/Domain/Entities/LocalUser.py
+++ password-manager_backend/Domain/Entities/LocalUser.py
@@ -1,14 +1,19 @@
 from sqlalchemy import Column, String, UUID
+from sqlalchemy.orm import relationship
 import uuid
-from .Base import Base
+from Domain.Entities.Base import Base
 
 class LocalUser(Base):
     __tablename__ = 'local_users'
 
     Id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
-    IdKeycloak = Column(UUID, unique=True, nullable=False, default=uuid.uuid4,)
-    HashMasterPassword = Column(String, nullable=False)  # colonna per hash password
-    SaltArgon = Column(String, nullable=False)           # colonna per salt
+    IdKeycloak = Column(UUID, unique=True, nullable=False, default=uuid.uuid4)
+    HashMasterPassword = Column(String, nullable=False)
+    SaltArgon = Column(String, nullable=False)
 
-    def __repr__(self):
-        return f"<LocalUser(IdKeycloak={self.IdKeycloak})>"
+    SubAccounts = relationship(
+        "SubAccount",
+        backref="LocalUser",
+        cascade="all, delete-orphan",
+        passive_deletes=True
+    )
--- password-manager_backend/Domain/Entities/Subaccount.py
+++ password-manager_backend/Domain/Entities/Subaccount.py
@@ -7,7 +7,7 @@ class SubAccount(Base):
     __tablename__ = "sub_accounts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("local_users.Id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("local_users.Id", ondelete="CASCADE"), nullable=False)
     title = Column(String(100), nullable=False) 
     username = Column(String(100), nullable=True)
     password_encrypted = Column(String, nullable=False)  
