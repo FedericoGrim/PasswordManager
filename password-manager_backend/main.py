3b9e9544e78c85bc5eaa18419471048328a7c2f2
FedericoGrim <federico.grimaldi2006@gmail.com>
2025-07-07 22:21:00 +0200

    Add DB session middleware to FastAPI app

--- password-manager_backend/main.py
+++ password-manager_backend/main.py
@@ -1,8 +1,10 @@
 # main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Request
+
 from config import Container
 from Presentation.Controllers.V1.LocalUserController import routerV1 as local_user_router_V1
 from Presentation.Controllers.V1.SubAccountController import routerV1 as subaccount_router_V1
+from Infrastructure.Repositories.Database import SessionLocal
 
 container = Container()
 container.wire(modules=["Presentation.Controllers.V1.LocalUserController", "Presentation.Controllers.V1.SubAccountController"])
@@ -23,3 +25,17 @@ app.include_router(
     prefix="/api/V1/subaccount",
     tags=["SubAccount"]
 )
+
+@app.middleware("http")
+async def DbSessionMiddleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        request.state.db = SessionLocal()
+        response = await call_next(request)
+        request.state.db.commit()
+    except Exception as e:
+        request.state.db.rollback()
+        raise e
+    finally:
+        request.state.db.close()
+    return response
