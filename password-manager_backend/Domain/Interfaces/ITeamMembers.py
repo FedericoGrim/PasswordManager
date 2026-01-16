280a3b4795c6bd8a23a2424908dfeac2beeb5954
FedericoGrim <federico.grimaldi2006@gmail.com>
2026-01-16 18:13:22 +0100

    Team members DB integration

---
+++ password-manager_backend/Domain/Interfaces/ITeamMembers.py
@@ -0,0 +1,26 @@
+import uuid
+from abc import ABC, abstractmethod
+
+from Domain.Entities.TeamMembers import TeamMembers
+
+@abstractmethod
+class ITeamMembers(ABC):
+    @abstractmethod
+    def AddMemberToTeam(self, member_id: uuid.UUID, team_id: uuid.UUID, role: str) -> dict:
+        pass
+
+    @abstractmethod
+    def GetTeamsByMemberId(self, member_id: uuid.UUID) -> list:
+        pass
+
+    @abstractmethod
+    def GetMembersByTeamId(self, team_id: uuid.UUID) -> list:
+        pass
+
+    @abstractmethod
+    def UpdateMemberRole(self, member_id: uuid.UUID, team_id: uuid.UUID, new_role: str) -> dict:
+        pass
+
+    @abstractmethod
+    def RemoveMemberFromTeam(self, member_id: uuid.UUID, team_id: uuid.UUID) -> dict:
+        pass
