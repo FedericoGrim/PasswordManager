f3d9fffdf91fb739afddd685511344e61964754c
FedericoGrim <federico.grimaldi2006@gmail.com>
2026-02-08 16:00:15 +0100

    Right font for the KEYDEN title in the home

--- password-manager_frontend/src/app/home/page.tsx
+++ password-manager_frontend/src/app/home/page.tsx
@@ -3,21 +3,25 @@
 import FavMenu from "./page-components/favMenu/favMenu";
 import Menu from "./page-components/menu/menu";
 
+import { Poppins } from "next/font/google";
 import "./styles.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
     <section className="flex flex-1 gap-0">
-      {/* Colonna sinistra */}
       <div className="flex-1 flex flex-col overflow-auto">
-        <h1 className="fav-title">Keyden</h1>
+        <h1 className={`fav-title ${poppins.className}`}>KEYDEN</h1>
         <FavMenu />
       </div>
 
-      {/* Divider */}
       <div className="divider"></div>
 
-      {/* Colonna destra */}
       <div className="flex-1 overflow-auto">
         <Menu />
       </div>
