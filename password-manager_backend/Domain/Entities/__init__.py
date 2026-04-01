7374156e282809cd73d149f5c2c4809845bdedde
Federico Grimaldi <142935058+FedericoGrim@users.noreply.github.com>
2026-04-01 15:27:28 +0200

    Removal of double folders and corrections of some folders naming convention

--- password-manager_backend/Domain/Entities/__init__.py
+++
@@ -1,7 +0,0 @@
-from Domain.Entities.Base import Base
-from Domain.Entities.User import User
-from Domain.Entities.SubAccount import SubAccount
-from Domain.Entities.Team import Team
-from Domain.Entities.TeamMembers import TeamMembers
-from Domain.Entities.Categories import Categories
-from Domain.Entities.SubAccountCategories import SubAccountCategories
